66ee973e42975b9a253049ad62a8f1baa711869f
dhencios15 <ddelacruz@umindanao.edu.ph>
2020-08-27 07:12:58 +0800

    remove mongo

--- routes/userRoutes.js
+++
@@ -1,9 +0,0 @@
-const { Router } = require('express');
-const { signup, signin } = require('../controllers/authController');
-
-const router = Router();
-
-router.post('/signup', signup);
-router.post('/signin', signin);
-
-module.exports = router;
